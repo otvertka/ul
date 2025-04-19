1b86443dcf8ec9b70e819e129e511ca361d69078
otvertka <otvertka123@ukr.net>
2025-04-19 09:15:15 +0200

    add Bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,8 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 // import path from "path";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
+
 import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({
@@ -26,5 +28,6 @@ export function buildPlugins({
 
         new webpack.HotModuleReplacementPlugin(),
         new ReactRefreshPlugin(),
+        new BundleAnalyzerPlugin({openAnalyzer: false})
     ];
 }
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,10 @@
-import { BugButton } from "app/providers/ErrorBoundary";
 import { useTranslation } from "react-i18next"
-
-
 
 const MainPage = () => {
 
     const { t } = useTranslation('main');
     return (
-        <div>
-            <BugButton/>            
+        <div> 
             {t('Startseite')}
         </div>
     )
