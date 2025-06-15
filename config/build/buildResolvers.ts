5a7e884c958a27b3ead5d32b6c2183f60365367d
otvertka <otvertka123@ukr.net>
2025-06-15 13:55:04 +0200

    add Redux store, add first entity

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -7,6 +7,8 @@ export function buildResolvers(options: BuildOptions): ResolveOptions {
         preferAbsolute: true,
         modules: [options.paths.src, "node_modules"],
         mainFiles: ["index"],
-        alias: {},
+        alias: {
+            entities: path.resolve(options.paths.src, "entities")
+        },
     };
 }
