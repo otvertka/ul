b976aa1b230f9af5a7a0cde8cadaeb6e30f18ad0
otvertka <otvertka123@ukr.net>
2025-06-15 13:55:50 +0200

    add Redux store, add first entity

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,5 +1,7 @@
 import { ResolveOptions } from "webpack";
 import { BuildOptions } from "./types/config";
+import path from "path";
+
 
 export function buildResolvers(options: BuildOptions): ResolveOptions {
     return {
